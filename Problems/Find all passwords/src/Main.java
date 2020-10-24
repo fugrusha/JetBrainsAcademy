7109b88f68777d07a49f8c9a1755ce963437d739
fugrusha <andrew.golowko@gmail.com>
2020-10-24 20:34:21 +0300

    Match results - Task 3

---
+++ Problems/Find all passwords/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("((?<=(password):?)\\s*\\w+)", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            matcher.reset();
+            while (matcher.find()) {
+                System.out.println(matcher.group().trim());
+            }
+        } else {
+            System.out.println("No passwords found.");
+        }
+    }
+}
