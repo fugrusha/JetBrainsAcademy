2a134b71ac667a6e1576d8dddc5096838ad202f9
fugrusha <andrew.golowko@gmail.com>
2020-10-26 21:42:12 +0200

    Abstract class - Task 1

---
+++ Problems/Shape hierarchy/src/Main.java
@@ -0,0 +1,69 @@
+abstract class Shape {
+
+    abstract double getPerimeter();
+
+    abstract double getArea();
+}
+
+class Triangle extends Shape {
+
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    @Override
+    double getPerimeter() {
+        return a + b + c;
+    }
+
+    @Override
+    double getArea() {
+        double area = (a + b + c) / 2.0d;
+        return Math.sqrt(area * (area - a) * (area - b) * (area - c));
+    }
+}
+
+class Rectangle extends Shape {
+
+    private double a;
+    private double b;
+
+    public Rectangle(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 2 * (a + b);
+    }
+
+    @Override
+    double getArea() {
+        return a * b;
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    double getArea() {
+        return Math.PI * radius * radius;
+    }
+}
